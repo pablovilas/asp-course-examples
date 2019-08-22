2ce159af6317b055933448cd08aa7200ea6aeaa5
Pablo Vilas <pablovilas89@gmail.com>
2019-08-22 20:09:53 -0300

    Adds Heroku support for sinatra example

--- s1c2/sinatra/sinatra.rb
+++ s1c2/sinatra/sinatra.rb
@@ -1,17 +1,19 @@
 require 'sinatra'
 require 'sinatra/json'
 
-# render text
-get '/hello' do
-    'Hello from ASP!'
-end
+class App < Sinatra::Base
+    # render text
+    get '/hello' do
+        'Hello from ASP!'
+    end
 
-# render json
-get '/hello.json' do
-    json message: 'Hello from ASP!'
-end
+    # render json
+    get '/hello.json' do
+        json message: 'Hello from ASP!'
+    end
 
-# render html
-get '/hello.html' do
-    send_file 'index.html'
+    # render html
+    get '/hello.html' do
+        send_file 'index.html'
+    end
 end
