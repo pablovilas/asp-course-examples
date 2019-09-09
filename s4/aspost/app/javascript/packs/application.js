a22dd539b7fa08e6479049bb4bd8506a35c77763
Pablo Vilas <pablovilas89@gmail.com>
2019-09-08 22:52:20 -0300

    C4: Adds bootstrap + webpacker

--- s4/aspost/app/javascript/packs/application.js
+++ s4/aspost/app/javascript/packs/application.js
@@ -8,6 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+let jQuery = require('jquery')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+import './bootstrap.js'
+import './stylesheets/bootstrap.scss'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
