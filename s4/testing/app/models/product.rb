79b69da2d0d6ef0986d6cd88c620f22b9b867be1
Pablo Vilas <pablovilas89@gmail.com>
2019-09-12 16:44:58 -0300

    C5: Adds practical example

--- s4/testing/app/models/product.rb
+++ s4/testing/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
     # name price
+    validates :name, presence: true, uniqueness: true
+    validates :price, numericality: { greater_than: 0 }
 end
--- s4/testing/test/models/product_test.rb
+++ s4/testing/test/models/product_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @product = products(:valid_drink)
+  end
+  
+  test 'with valid price and name validation succeeds' do
+    assert @product.valid?
+  end
+
+  test 'with valid price but same name validation fails' do
+    new_product = Product.new(@product.attributes)
+    assert !new_product.valid?
+  end
+
+  test 'with invalid price validation fails' do
+    @product.price = -10
+    assert !@product.valid?
+  end
+
+  test 'with valid floar price validation succeeds' do
+    @product.price = 10.15
+    assert @product.valid?
+  end
+
 end
