3b1e1b0139e5886fccb6b41a80f8a2a3e6a530fe
Pablo Vilas <pablovilas89@gmail.com>
2019-09-17 23:38:50 -0300

    S5C1: Adds phone book example views and seeds

--- s5/s5c1/phone-book/db/seeds.rb
+++ s5/s5c1/phone-book/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+    company = Company.new(:name => Faker::Company.name)
+    if company.save
+        SecureRandom.random_number(300).times do
+        company.employees.create(
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            phone_number: Faker::PhoneNumber.phone_number
+        )
+        end
+    end
+end
