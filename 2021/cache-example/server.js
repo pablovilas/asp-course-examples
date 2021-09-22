ce9aceddb625489a733141bb113bed7e45daf6e1
Pablo Vilas <pablovilas89@gmail.com>
2021-09-22 11:11:24 -0300

    Cache example + CDN

---
+++ 2021/cache-example/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.sendFile(`${__dirname}/static/index.html`);
+});
+
+app.get('/app', (req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.sendFile(`${__dirname}/static/index.html`);
+});
+
+app.get('/no-cache/:id', (req, res) => {
+  let stream = fs.createReadStream(`./static/${req.params.id}.jpg`);
+  stream.on('open', function () {
+    res.set('Content-Type', 'image/jpeg');
+    stream.pipe(res);
+  });
+});
+
+app.get('/cache/:id', (req, res) => {
+  let stream = fs.createReadStream(`./static/${req.params.id}.jpg`);
+  stream.on('open', function () {
+    res.set('Content-Type', 'image/jpeg');
+    res.set('Cache-Control', 'private, max-age=3600'); // Only for end user, 1 hour cache
+    stream.pipe(res);
+  });
+});
+
+app.get('/cdn/:id', (req, res) => {
+  let stream = fs.createReadStream(`./static/${req.params.id}.jpg`);
+  stream.on('open', function () {
+    res.set('Content-Type', 'image/jpeg');
+    res.set('Cache-Control', 'public, max-age=3600'); // CDN available, 1 hour cache
+    stream.pipe(res);
+  });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
