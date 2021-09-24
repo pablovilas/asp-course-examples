9859bae8269383e281845ca76b85f90f7fcfad58
Pablo Vilas <pablovilas89@gmail.com>
2021-09-24 08:00:52 -0300

    Remove unecessary endpoints

--- 2021/cache-example/server.js
+++ 2021/cache-example/server.js
@@ -10,11 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/static/index.html`);
 });
 
-app.get('/app', (req, res) => {
-  res.set('Content-Type', 'text/html');
-  res.sendFile(`${__dirname}/static/index.html`);
-});
-
 app.get('/no-cache/:id', (req, res) => {
   let stream = fs.createReadStream(`./static/${req.params.id}.jpg`);
   stream.on('open', function () {
@@ -28,15 +23,6 @@ app.get('/cache/:id', (req, res) => {
   stream.on('open', function () {
     res.set('Content-Type', 'image/jpeg');
     res.set('Cache-Control', 'private, max-age=3600'); // Only for end user, 1 hour cache
-    stream.pipe(res);
-  });
-});
-
-app.get('/cdn/:id', (req, res) => {
-  let stream = fs.createReadStream(`./static/${req.params.id}.jpg`);
-  stream.on('open', function () {
-    res.set('Content-Type', 'image/jpeg');
-    res.set('Cache-Control', 'public, max-age=3600'); // CDN available, 1 hour cache
     stream.pipe(res);
   });
 });
